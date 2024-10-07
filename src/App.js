632ed8e574fe59ae9c35b25f38cbfd2b288d1cee
Facundo Pepino <fpepino@facultad.sanfrancisco.utn.edu.ar>
2024-10-07 20:13:44 -0300

    Uso de map para iterar productos encontrados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [listOfProducts, setListOfProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const request = {
+    method: 'GET',
+    headers: {'Content-Type' : 'application/json'}
+  }
+
+  function onSubmitSearch(e) {
+    e.preventDefault();
+    const formData_Item = new FormData(e.target); // Crea un objeto FormData para obtener los datos del formulario
+    let query = formData_Item.get("search"); // Obtiene el valor del campo "nameItem"
+
+    setLoading(true);
+      fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`, request)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Error en la solicitud: ${response.status}`)
+        else {
+          setError(null);
+          setLoading(false); 
+          return response.json()
+        }
+      })
+      .then((data) => setListOfProducts(data.results))
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+        setListOfProducts(null); 
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmitSearch}>
+        <input name='search' id='search' placeholder='Buscar productos...'></input>
+        <button>Buscar</button>
+      </form>
+
+      {
+        listOfProducts && (
+          <div>
+            {listOfProducts.map((product) => (
+              <div key={product.id}>
+                <h3>{product.title}</h3>
+                <h3>{product.price}</h3>
+                <img src={product.thumbnail} alt={product.title} />
+              </div>
+            ))}
+          </div>
+        )
+      }
+
+      {error && <p>{error.message}</p>}
+      {loading && <p>Cargando...</p>}
+
     </div>
   );
 }
