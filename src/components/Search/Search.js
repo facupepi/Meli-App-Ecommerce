cec8afa3d478120e83517175f682baa68b1f766d
Santiago Villalba <santiagovillalba-2002@hotmail.com>
2024-10-15 00:15:15 -0300

    estilos aplicados

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -8,23 +8,22 @@ function Search({products, loading}) {
         :
         (
             <div className="container">
-                <h2>Productos encontrados</h2>
+                <h2 className='text-center'>Productos encontrados</h2>
                 {products
                     ? (
                         <div className="product-list">
                             
                             {products.map((product) => (
                                 <div key={product.id} className="product-item">
-                                    <img src={product.thumbnail} alt={product.title} className="product-image"/>
+                                    <div className='product-thumbnail'>
+                                        <img src={product.thumbnail} alt={product.title} className="product-image"/>
+                                    </div>
                                     <div className="product-info">
                                         <h3 className="product-title">{product.title}</h3>
                                         <p className="product-price">${product.price}</p>
                                         <p className="product-store">Por {product.seller
                                                 ?.nickname}</p>
-                                    </div>
-
-                                    <div className="product-buttons">
-                                        <Link to={`/product/${product.id}`} className="btn-detail">Ver Detalle</Link>
+                                                <Link to={`/product/${product.id}`} className="btn-detail">Ver Detalle</Link>
                                         <button className="btn-add-cart">Agregar al Carrito</button>
                                     </div>
                                 </div>
